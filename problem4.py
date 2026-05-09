bb45e5dd38c0b9c24614bda3941837504698530e
Deepu mahapatro <deepumahapatro2006@gmail.com>
2026-05-09 10:38:45 +0530

    Clean up problem4.py by removing commented code
    Refactor code to remove comments and improve readability.

--- problem4.py
+++ problem4.py
@@ -1,15 +1,16 @@
-# p=int(input("enter a number"))
-# t=int(input("enter a number "))
-# r=int(input("enter rate "))
-# s=(p*t*r)/100
-# print(s)
+#using normal method
+p=int(input("enter a number"))
+t=int(input("enter a number "))
+r=int(input("enter rate "))
+s=(p*t*r)/100
+print(s)
 
 
 #using function
-# def simple(p,t,r):
-#     return (p*t*r)/100
-# s=simple(8,6,8)
-# print(s)
+ def simple(p,t,r):
+     return (p*t*r)/100
+ s=simple(8,6,8)
+ print(s)
 
 
 #using lamda function
