b11b2ac42b2af45b434bccdccac282e1c0628886
deepu <deepumahapatro2006@gmail.com>
2026-05-16 11:36:48 +0530

    EVEN NUMBERS IN A LIST

---
+++ problem30.py
@@ -0,0 +1,28 @@
+#EVEN NUMBERS IN A LIST
+
+#USING LOOP METHOD
+num=[1,2,3,4,5,6,7,8,9]
+for i in num:
+    if i%2==0:
+        print("even number")
+        break
+else:
+    print("not a even number")
+    
+    
+#USING FILTER METHOD() 
+num=[1,2,3,4,5,6,7]
+result=list(filter(lambda x: x%2==0,num))
+print(result)
+
+
+#USING LIST COMPREHENSION
+num=[1,2,3,4,5,6,7]
+result=[x for x in num if x%2==0]
+print(result)
+
+
+#USING BITWISE OPERATOR
+num=[12,3,4,5,6,7,8]
+result=[x for x in num if x&1 ==0]
+print(result)
